f1e8f4b650ff741bfb5b3c90ed1ad34d1ba2cd34
JohnPKosh <jkosh@broadvox.com>
2014-12-10 11:57:05 -0500

    Adding ObjectBase to source

---
+++ IODataBlock/Business/Business.Common/System/ObjectBase.cs
@@ -0,0 +1,49 @@
+﻿using Business.Common.Extensions;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace Business.Common.System
+{
+    public class ObjectBase<T> : IObjectBase
+    {
+        public static T CreateFromJson(string value)
+        {
+            return value.ConvertJson<T>();
+        }
+
+        public static T CreateFromJson(string value, params JsonConverter[] converters)
+        {
+            return value.ConvertJson<T>(converters);
+        }
+
+        public static T CreateFromJson(string value, JsonSerializerSettings settings)
+        {
+            return value.ConvertJson<T>(settings);
+        }
+
+        public static T CreateFromExpando(dynamic value)
+        {
+            return (value as ExpandoObject).ConvertExpandoTo<T>();
+        }
+
+        public static T CreateFromExpando(ExpandoObject value)
+        {
+            return value.ConvertExpandoTo<T>();
+        }
+
+        public string ToJson(bool indented = false)
+        {
+            return JsonConvert.SerializeObject(this, indented ? Formatting.Indented : Formatting.None);
+        }
+
+        public void PopulateFromJson(string value)
+        {
+            this.PopulateObjectFromJson(value);
+        }
+
+        public void PopulateFromJson(string value, object target, JsonSerializerSettings settings)
+        {
+            this.PopulateObjectFromJson(value, settings);
+        }
+    }
+}
