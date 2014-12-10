cd3394c933b213e4761bed1244b53c6ca67043ce
JohnPKosh <jkosh@broadvox.com>
2014-12-10 11:48:27 -0500

    Adding Business Common Code

---
+++ IODataBlock/Business/Business.Common/System/IObjectBase.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Business.Common.System
+{
+    public interface IObjectBase
+    {
+        String ToJson(Boolean indented = false);
+
+        void PopulateFromJson(String value);
+
+        void PopulateFromJson(string value, object target, JsonSerializerSettings settings);
+    }
+}
---
+++ IODataBlock/Test/Business.Test/Utility/FakePet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Common.Extensions;
+using Business.Common.System;
+using Newtonsoft.Json;
+
+namespace Business.Test.Utility
+{
+    public class FakePet : ObjectBase<FakePet>
+    {
+        //public static FakePet CreateFromJson(String json)
+        //{
+        //    return FromJsonString<FakePet>(json);
+        //}
+
+
+        public static FakePet CreateBela()
+        {
+            var fido = new FakePet {Name = "Bela", Age = 6, NickName = "Dummee"};
+            return fido;
+        }
+
+        public static FakePet CreateNala()
+        {
+            var fido = new FakePet { Name = "Nala", Age = 2, NickName = "Dumb Dumb Doo Doo Head" };
+            return fido;
+        }
+
+        public String Name { get; set; }
+        public Int32 Age { get; set; }
+        public String NickName { get; set; }
+    }
+}
