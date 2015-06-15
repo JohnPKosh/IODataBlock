13d0ed21051cfaca298d8e5542f9a91ef82e515f
John Kosh <devprowork@live.com>
2015-06-14 22:47:26 -0400

    Added Bootstrap Less

--- IODataBlock/Sandbox/App_Start/BundleConfig.cs
+++ IODataBlock/Sandbox/App_Start/BundleConfig.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Optimization;
+﻿using BundleTransformer.Core.Bundles;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
+using System.Web.Optimization;
 
 namespace Sandbox
 {
@@ -7,24 +10,68 @@ namespace Sandbox
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.UseCdn = true;
+            var cssTransformer = new StyleTransformer();
+            var jsTransformer = new ScriptTransformer();
+            var nullOrderer = new NullOrderer();
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            var cssBundle = new StyleBundle("~/bundles/css");
+            cssBundle.Include("~/Content/Site.less", "~/Content/bootstrap/bootstrap.less");
+            cssBundle.Transforms.Add(cssTransformer);
+            cssBundle.Orderer = nullOrderer;
+            bundles.Add(cssBundle);
+
+            var jqueryBundle = new ScriptBundle("~/bundles/jquery");
+            jqueryBundle.Include("~/Scripts/jquery-{version}.js");
+            jqueryBundle.Transforms.Add(jsTransformer);
+            jqueryBundle.Orderer = nullOrderer;
+            bundles.Add(jqueryBundle);
+
+            var jqueryvalBundle = new ScriptBundle("~/bundles/jqueryval");
+            jqueryvalBundle.Include("~/Scripts/jquery.validate*");
+            jqueryvalBundle.Transforms.Add(jsTransformer);
+            jqueryvalBundle.Orderer = nullOrderer;
+            bundles.Add(jqueryvalBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            var modernizrBundle = new ScriptBundle("~/bundles/modernizr");
+            modernizrBundle.Include("~/Scripts/modernizr-*");
+            modernizrBundle.Transforms.Add(jsTransformer);
+            modernizrBundle.Orderer = nullOrderer;
+            bundles.Add(modernizrBundle);
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-        }
-    }
+            var bootstrapBundle = new ScriptBundle("~/bundles/bootstrap");
+            bootstrapBundle.Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js");
+            bootstrapBundle.Transforms.Add(jsTransformer);
+            bootstrapBundle.Orderer = nullOrderer;
+            bundles.Add(bootstrapBundle);
+      }
+
+
+        //// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        //public static void RegisterBundles(BundleCollection bundles)
+        //{
+        //    bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+        //                "~/Scripts/jquery-{version}.js"));
+
+        //    bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+        //                "~/Scripts/jquery.validate*"));
+
+        //    // Use the development version of Modernizr to develop with and learn from. Then, when you're
+        //    // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+        //    bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+        //                "~/Scripts/modernizr-*"));
+
+        //    bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+        //              "~/Scripts/bootstrap.js",
+        //              "~/Scripts/respond.js"));
+
+        //    bundles.Add(new StyleBundle("~/Content/css").Include(
+        //              "~/Content/bootstrap.css",
+        //              "~/Content/site.css"));
+        //}
+
+  }
 }
