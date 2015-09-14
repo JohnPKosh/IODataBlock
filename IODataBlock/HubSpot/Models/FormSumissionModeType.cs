a8e14e70373941fc7947b7a0a5dc4b51b6603623
John Kosh <devprowork@live.com>
2015-09-14 10:23:58 -0400

    Fixing to be able to deploy to Azure.

---
+++ IODataBlock/HubSpot/Models/FormSumissionModeType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace HubSpot.Models
+{
+    public enum FormSumissionModeType
+    {
+        Newest, All, None, Oldest
+    }
+}
---
+++ IODataBlock/HubSpot/Services/IContactService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using HubSpot.Models;
+
+namespace HubSpot.Services
+{
+    /* http://developers.hubspot.com/docs/methods/contacts/contacts-overview */
+
+    public interface IContactService
+    {
+        /* http://developers.hubspot.com/docs/methods/contacts/create_contact */
+        /* Example URL to POST to:  https://api.hubapi.com/contacts/v1/contact/?hapikey=demo */
+
+        string CreateContact(string value);
+
+        string CreateContact(ContactDto value);
+
+        /* http://developers.hubspot.com/docs/methods/contacts/update_contact */
+        /* Example URL to POST to:  https://api.hubapi.com/contacts/v1/contact/vid/61571/profile?hapikey=demo */
+
+        bool UpdateContact(string value, int id);
+
+        bool UpdateContact(ContactDto value, int id);
+
+        /* http://developers.hubspot.com/docs/methods/contacts/create_or_update */
+        /* Example URL to POST to:  http://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/test@hubspot.com/?hapikey=demo */
+
+        string UpsertContact(string value);
+
+        string UpsertContact(ContactDto value);
+
+        /* http://developers.hubspot.com/docs/methods/contacts/batch_create_or_update */
+        /* Example URL to POST to:  "http://api.hubapi.com/contacts/v1/contact/batch/?hapikey=demo" */
+
+        string BatchUpsertContacts(string value);
+
+        string BatchUpsertContacts(IEnumerable<ContactDto> value);
+
+        /* http://developers.hubspot.com/docs/methods/contacts/delete_contact */
+        /* Example URL:  https://api.hubapi.com/contacts/v1/contact/vid/61571?hapikey=demo */
+
+        bool DeleteContact(int id);
+
+        /* http://developers.hubspot.com/docs/methods/contacts/delete_contact */
+        /* Example URL:  https://api.hubapi.com/contacts/v1/contact/vid/61571?hapikey=demo */
+
+        bool GetAllContacts();
+    }
+}
