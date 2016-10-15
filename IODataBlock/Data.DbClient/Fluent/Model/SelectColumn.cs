41e4ddd2131284ee90986e5435df5271a1bba0a7
John <devprowork@live.com>
2016-10-15 07:29:28 -0400

    Adding Select Column type to project.

---
+++ IODataBlock/Data.DbClient/Fluent/Model/SelectColumn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Common.System;
+using Data.DbClient.Fluent.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Data.DbClient.Fluent.Model
+{
+    public class SelectColumn : ObjectBase<QueryObjectBase>
+    {
+        public string SelectItem { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Prefix { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Alias { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public SelectColumnType ColumnType { get; set; }
+    }
+}
